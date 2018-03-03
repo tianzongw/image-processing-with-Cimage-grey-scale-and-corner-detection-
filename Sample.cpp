deea1b56847a15a29f684e0f5f3e32cc92f8fe34
WANG Tianzong (Logan) <34018762+loganwangtz@users.noreply.github.com>
2018-03-03 17:25:24 +0800

    Update and rename Assignment2.cpp to Sample.cpp

--- Sample.cpp
+++ Sample.cpp
@@ -38,7 +38,7 @@ void CCorner::RGBToGrayScale(CImage* pIn, CImage* pOut)
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
 			pIn->GetRGB(i, j, &r, &g, &b);
-			grey = 0.3*(int)r + 0.59*(int)g + 0.11*(int)b;
+			grey = 0.299*(int)r + 0.587*(int)g + 0.114*(int)b;
 			pOut->SetIndex(i, j, grey);
 		}
 	}
@@ -69,37 +69,25 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 
 	////
 	// Step 1: Compute a proper size for Gaussian filter
-		cin >> sigma;
+		//cin >> sigma;
 		int size = (int)sigma*(pow(2 * log(1000), 0.5));
 
 	////
 	// Step 2: Define the Gaussian filter and partial filter
 		//define Guassian filter
 			double *gaus = new double[size];
-			//for (int i = 0; i<size; i++)
-			//{
-			//	gaus[i] = new double[size];   
-			//}
-		
 			int center = size / 2;
 			double sum = 0;
 
 			for (int i = 0; i < size; i++){
-				gaus[i] = exp(-(i - center)*(i - center) / (2 * sigma*sigma));					
+				gaus[i] = exp(-(i - center)*(i - center) / (2 * sigma*sigma));
+				sum += gaus[i];
 			}
-			//for (int i = 0; i < size; i++) {
-			//	for (int j = 0; j < size; j++) {
-			//		gaus[i][j] = (1 / (2 * PI*sigma*sigma))*exp(-((i - center)*(i - center) + (j - center)*(j - center)) / (2 * sigma*sigma));
-			//		sum += gaus[i][j];
-			//	}
-			//}
-
-			//or (int i = 0; i < size; i++) {
-			//	for (int j = 0; j < size; j++) {
-			//		gaus[i][j] /= sum;
-			//	}
-			//}
-
+			
+			for (int i = 0; i < size; i++) {
+					gaus[i] /= sum;
+				}
+			
 		//define partial filter
 		//	double *partial = new double[3];
 		//	partial[0] = 0.5;
@@ -113,7 +101,7 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 
 		//calculate Ix
 			double **Ix = new double *[height];
-			for (int i = 0; i<size; i++)
+			for (int i = 0; i<height; i++)
 			{
 				Ix[i] = new double[width];
 			}
@@ -121,34 +109,36 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 			for (int i = 0; i < height; i++) {
 				for (int j = 0; j < width; j++) {
 					if (j == 0) {
-						Ix[i][j] = pIn->GetIndex(i, j + 1) - pIn->GetIndex(i, j);
+						Ix[i][j] = pIn->GetIndex(j + 1, i) - pIn->GetIndex(j, i);
 					}
 					else if (j == width - 1) {
-						Ix[i][j] = pIn->GetIndex(i, j) - pIn->GetIndex(i, j - 1);
+						Ix[i][j] = pIn->GetIndex(j, i) - pIn->GetIndex(j - 1, i);
 					}
 					else {
-						Ix[i][j] = 0.5*pIn->GetIndex(i, j - 1) - 0.5*pIn->GetIndex(i, j + 1);
+						Ix[i][j] = 0.5*pIn->GetIndex(j + 1, i) - 0.5*pIn->GetIndex(j - 1, i);
 					}
 				}
 			}
 
+			
+
 		//calculate Iy
 			double **Iy = new double *[height];
-			for (int i = 0; i<size; i++)
+			for (int i = 0; i<height; i++)
 			{
 				Iy[i] = new double[width];
 			}
 
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
 					if (i == 0) {
-						Iy[i][j] = pIn->GetIndex(i + 1,j) - pIn->GetIndex(i, j);
+						Iy[i][j] = pIn->GetIndex(j, i + 1) - pIn->GetIndex(j, i);
 					}
 					else if (i == height - 1) {
-						Ix[i][j] = pIn->GetIndex(i, j) - pIn->GetIndex(i - 1, j);
+						Iy[i][j] = pIn->GetIndex(j, i) - pIn->GetIndex(j, i - 1);
 					}
 					else {
-						Ix[i][j] = 0.5*pIn->GetIndex(i - 1, j) - 0.5*pIn->GetIndex(i + 1, j);
+						Iy[i][j] = 0.5*pIn->GetIndex(j, i + 1) - 0.5*pIn->GetIndex(j, i - 1);
 					}
 				}
 			}
@@ -167,8 +157,8 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 				Ixy[i] = new double[width];
 			}
 
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
 					Ixx[i][j] = pow(Ix[i][j], 2);
 					Iyy[i][j] = pow(Iy[i][j], 2);
 					Ixy[i][j] = Ix[i][j] * Iy[i][j];
@@ -177,33 +167,32 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 
 	////
 	// Step 5: Smooth Ixx, Iyy, Ixy
-			for (int i = 1; i < width; i++) {
-				for (int j = 1; j < height; j++) {
+			for (int i = 0; i < height - 1; i++) {
+				for (int j = center; j < width - center; j++) {
 					double temp1xx = 0.0;
 					double temp1yy = 0.0;
 					double temp1xy = 0.0;
 					//1DConvolution wrt x
 					for (int k = 0; k < size; k++) {
-						if (j - center >= 0) {
-							temp1xx += Ixx[i][j + (k - center)] * gaus[k];
-							temp1yy += Iyy[i][j + (k - center)] * gaus[k];
-							temp1xy += Ixy[i][j + (k - center)] * gaus[k];
+						//if (j - center >= 0) {
+						temp1xx += Ixx[i][j + (k - center)] * gaus[k];
+						temp1yy += Iyy[i][j + (k - center)] * gaus[k];
+						temp1xy += Ixy[i][j + (k - center)] * gaus[k];
 						}
-						Ixx[i][j] = temp1xx;
-						Iyy[i][j] = temp1yy;
-						Ixy[i][j] = temp1xy;
+					Ixx[i][j] = temp1xx;
+					Iyy[i][j] = temp1yy;
+					Ixy[i][j] = temp1xy;
 					}
 				}
-			}
 
-			for (int j = 1; j < height; j++) {
-				for (int i = 1; i < width; i++) {
+			for (int i = center; i < height - center; i++) {
+				for (int j = 0; j < width - 1; j++) {
 					double temp2xx = 0.0;
 					double temp2yy = 0.0;
 					double temp2xy = 0.0;
 					//1DConvolution wrt y
 					for (int k = 0; k < size; k++) {
-						if (i - center >= 0) {
+						//if (i - center >= 0) {
 							temp2xx += Ixx[i + (k - center)][j] * gaus[k];
 							temp2yy += Iyy[i + (k - center)][j] * gaus[k];
 							temp2xy += Ixy[i + (k - center)][j] * gaus[k];
@@ -214,7 +203,6 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 
 					}
 				}
-			}
 	////
 	// Step 6: Compute R
 			double **R = new double *[height];
@@ -223,17 +211,18 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 			}
 
 			double k = 0.04;	
-			for (int i = 0; i < width; i++) {
-				for (int j = 0; j < height; j++) {
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
 					R[i][j] = (Ixx[i][j] * Iyy[i][j] - Ixy[i][j] * Ixy[i][j]) - 0.04*pow(Ixx[i][j]+Iyy[i][j],2);
 				}
 			}
 	////
 	// Step 7: Locate Maxima in R
 			vector<pair<int, int>> cordinatesR;
-			for (int i = 1; i < height; i++) {
-				for (int j = 1; j < width; j++) {
-					if (R[i][j] > max(R[i-1][j],R[i][j+1]) && R[i][j] > max(R[i+1][j], R[i][j-1])) {
+			for (int i = 1; i < height-1; i++) {
+				for (int j = 1; j < width-1; j++) {
+					double tmo = R[i][j];
+					if (R[i][j] > max(R[i-1][j],R[i][j+1]) && R[i][j] > max(R[i+1][j], R[i][j-1]) && R[i][j] > max(R[i + 1][j + 1], R[i + 1][j - 1]) && R[i][j] > max(R[i - 1][j - 1], R[i - 1][j + 1])) {
 						cordinatesR.push_back(make_pair(i, j));
 					}
 				}
@@ -241,27 +230,25 @@ void CCorner::ObtainCorners(CImage* pIn, double sigma, double threshold, vector<
 	////
 	// Step 8: Compute corner candidates up to sub-pixel accuracy and interpolate R value for corner candidates.
 			vector<pair<int, int>>::iterator itr;
-			vector<pair<int, int>> cordinatesCimg;
 			for (itr = cordinatesR.begin(); itr != cordinatesR.end(); itr++) {
 				double a = 0.5*(R[itr->first][itr->second - 1] + R[itr->first][itr->second + 1] - 2 * R[itr->first][itr->second]);
-				double b = 0.5*(R[itr->first - 1][itr->second] + R[itr->first + 1][itr->second] - 2 * R[itr->first][itr->second]);
+				double b = 0.5*(R[itr->first + 1][itr->second] + R[itr->first - 1][itr->second] - 2 * R[itr->first][itr->second]);
 				double c = 0.5*(R[itr->first][itr->second + 1] - R[itr->first][itr->second - 1]);
-				double d = 0.5*(R[itr->first + 1][itr->second] - R[itr->first - 1][itr->second]);
-				int x = -1 * c / (2 * a) + itr->first;
-				int y = -1 * d / (2 * b) + itr->second;
-				cordinatesCimg.push_back(make_pair(x, y));
+				double d = 0.5*(R[itr->first - 1][itr->second] - R[itr->first + 1][itr->second]);
+				double e = R[itr->first][itr->second];
+				double x = -1 * c / (2 * a);
+				double y = -1 * d / (2 * b);
+				double Rcal = a*x*x + b*y*y + c*x + d*y + e;
+				
+				// Step 9: Use the threshold value to identify strong corners for output
+					if (Rcal > threshold) {
+						C2DPoint* pPnt = new C2DPoint((x + itr->second), height - 1 - (y + itr->first));
+						//C2DPoint* pPnt = new C2DPoint(1000, 1000);
+						pResultCorners->push_back(pPnt);
+						}
+				////
 			}
-
-	////
-	// Step 9: Use the threshold value to identify strong corners for output
-
+	
 }
-	// OUTPUT:
-	//     vector<C2DPoint*>* pResultCorners:	
-	//                      A std::vector object that holds all detected corners. Each corner is represented by
-	//                      a C2DPoint structure. An example is given below, showing how a corner object is
-	//                      initialized and stored in pResultCorners:
-	//                      
-	//                      	C2DPoint* pPnt = new C2DPoint(x, y);
-	//                      	pResultCorners.push_back(pPnt);
+	
 
